c9539ff1b474c83b22dd9b252c34e4f3bf58ac1b
mariafernanda <mariafernandalibano345@gmail.com>
2024-10-11 08:42:11 -0300

    movimentando canos

--- JumpBird/MainPage.xaml.cs
+++ JumpBird/MainPage.xaml.cs
@@ -4,10 +4,65 @@ public partial class MainPage : ContentPage
 {
 
 
+	const int TempoEntreFrames = 25;
+	const int Gravidade = 3;
+	double LarguraJanela;
+	double AlturaJanela;
+	int velocidade = 20;
+	bool estaMorto = true;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
+  public async void Desenha()
+  {
+	while (!estaMorto)
+	{
+		AplicaGravidade();
+		GerenciaCanos();
+		await Task.Delay(TempoEntreFrames);
+	}
+  }
+	void AplicaGravidade()
+	{
+		Passaro.TranslationY += Gravidade;
+	
+	}
+
+	
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		LarguraJanela = w;
+		AlturaJanela = h;
+		
+	}
+	void GerenciaCanos()
+	{
+		imgCanoCima.TranslationX -= velocidade;
+		imgCanoBaixo.TranslationX -= velocidade;
+
+		if (imgCanoBaixo.TranslationX <- LarguraJanela)
+		{
+			imgCanoBaixo.TranslationX = 0;
+			imgCanoCima.TranslationX = 0;
+		}
+	}
+
+
+	void Ei(object s, TappedEventArgs e)
+	{
+		GameOverFrame.IsVisible = false;
+		estaMorto = false;
+		Inicializar();
+		Desenha();
+	}
+	void Inicializar()
+	{
+		Passaro.TranslationY = 0;
+	}
+
 
 }
 
